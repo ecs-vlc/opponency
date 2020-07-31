639e7923126057b3595eb264dcb33daf7abfddae
Ethan Harris <ewah1g13@soton.ac.uk>
2020-07-31 14:04:51 +0100

    Update mosaic training

--- training/train_all_mosaic.py
+++ training/train_all_mosaic.py
@@ -1,5 +1,5 @@
 """
-Train models with input channels shuffled
+Train models with input mosaics
 """
 import math
 from model import BaselineModel
@@ -13,11 +13,29 @@ from torch import optim
 from torch.utils.data import DataLoader
 from torchvision.datasets import CIFAR10
 import pathlib
+from sklearn.model_selection import ParameterGrid
+
+import argparse
+
+parser = argparse.ArgumentParser(description='Mosaic')
+parser.add_argument('--arr', default=0, type=int, help='point in job array')
+args = parser.parse_args()
 
 bottlenecks = [1, 2, 4, 8, 16, 32]
 ventral_depths = [0, 1, 2, 3, 4]
 n_trials = 10
 cmode = 'colour'
+
+param_grid = ParameterGrid({
+    'n_bn': bottlenecks,
+    'd_vvs': ventral_depths,
+    'rep': list(range(n_trials))
+})
+
+params = param_grid[args.arr]
+n_bn = params['n_bn']
+d_vvs = params['d_vvs']
+t = params['rep']
 
 nch = 3
 train_transform = transforms.Compose([
@@ -98,26 +116,26 @@ testset = Mosaic(CIFAR10(".", train=False, download=True, transform=test_transfo
 trainloader = DataLoader(trainset, batch_size=32, shuffle=True, num_workers=8)
 testloader = DataLoader(testset, batch_size=32, shuffle=True, num_workers=8)
 
-for n_bn in bottlenecks:
-    for d_vvs in ventral_depths:
-        for t in range(n_trials):
-            model_file = f'./models/{cmode}-mos/model_{n_bn}_{d_vvs}_{t}.pt'
-            log_file = f'./logs/{cmode}-mos/model_{n_bn}_{d_vvs}_{t}.csv'
+# for n_bn in bottlenecks:
+#     for d_vvs in ventral_depths:
+#         for t in range(n_trials):
+model_file = f'./models/{cmode}-mos/model_{n_bn}_{d_vvs}_{t}.pt'
+log_file = f'./logs/{cmode}-mos/model_{n_bn}_{d_vvs}_{t}.csv'
 
-            pathlib.Path(model_file).parents[0].mkdir(parents=True, exist_ok=True)
-            pathlib.Path(log_file).parents[0].mkdir(parents=True, exist_ok=True)
+pathlib.Path(model_file).parents[0].mkdir(parents=True, exist_ok=True)
+pathlib.Path(log_file).parents[0].mkdir(parents=True, exist_ok=True)
 
-            model = BaselineModel(n_bn, d_vvs, nch)
+model = BaselineModel(n_bn, d_vvs, nch)
 
-            optimiser = optim.RMSprop(model.parameters(), alpha=0.9, lr=0.0001, weight_decay=1e-6)
-            loss_function = nn.CrossEntropyLoss()
+optimiser = optim.RMSprop(model.parameters(), alpha=0.9, lr=0.0001, weight_decay=1e-6)
+loss_function = nn.CrossEntropyLoss()
 
-            device = "cuda:0" if torch.cuda.is_available() else "cpu"
+device = "cuda:0" if torch.cuda.is_available() else "cpu"
 
-            trial = Trial(model, optimiser, loss_function, metrics=['loss', 'accuracy'],
-                          callbacks=[
-                              # torchbearer.callbacks.imaging.MakeGrid().on_train().to_file('sample.png'),
-                              torchbearer.callbacks.csv_logger.CSVLogger(log_file)]).to(device)
-            trial.with_generators(trainloader, val_generator=testloader)
-            trial.run(epochs=20)
-            torch.save(model.state_dict(), model_file)
+trial = Trial(model, optimiser, loss_function, metrics=['loss', 'accuracy'],
+              callbacks=[
+                  # torchbearer.callbacks.imaging.MakeGrid().on_train().to_file('sample.png'),
+                  torchbearer.callbacks.csv_logger.CSVLogger(log_file)]).to(device)
+trial.with_generators(trainloader, val_generator=testloader)
+trial.run(epochs=20)
+torch.save(model.conv_dict(), model_file)
